52333b243c416109d71e2bf2e8b6b974ca748e0c
Sean O'Meara <49831134+seanomeara96@users.noreply.github.com>
2020-04-21 22:59:47 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,106 +1,104 @@
-"use strict";
-
-function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
-
-function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
 /*
   how many weeks youve been alive
   90 years = 4692.86 // 4693
 */
-var Calc = /*#__PURE__*/function () {
-  function Calc() {
-    _classCallCheck(this, Calc);
-
-    this.monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+console.clear();
+class Calc {
+  constructor(day = 13, month = 7, year = 1996) {
+    this.monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; //
     this.container = document.querySelector("#container");
+    this.form = document.querySelector(".form");
+    this.formInput = document.querySelector(".prompt__input");
     this.currentYear = new Date().getFullYear();
     this.currentMonth = new Date().getMonth();
     this.currentDate = new Date().getDate();
-    this.birthYear = 1996;
-    this.birthMonth = 6; // 7th base 0
 
-    this.birthDay = 13;
-    this.ageInYears = this.currentYear - this.birthYear;
-    var totalDays = this.sumOfDays(this.calcDaysBetween(), this.calcDaysToEOY(), this.calcDaysFromBOY());
-    this.fillBlocks(this.daysToWeeks(totalDays));
+    this.promptUser();
+    this.events();
+  }
+  events() {
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log(this.formInput.value);
+      let [year, month, day] = this.formInput.value.split("-").map((x) => {
+        return parseInt(x);
+      });
+      this.birthYear = year;
+      this.birthMonth = month - 1;
+      this.birthDay = day;
+      this.ageInYears = this.currentYear - this.birthYear;
+      let totalDays = this.sumOfDays(
+        this.calcDaysBetween(),
+        this.calcDaysToEOY(),
+        this.calcDaysFromBOY()
+      );
+      this.promptUser();
+      this.fillBlocks(this.daysToWeeks(totalDays));
+    });
+  }
+  promptUser() {
+    this.form.classList.toggle("form--is-visible");
   }
 
-  _createClass(Calc, [{
-    key: "calcDaysBetween",
-    value: function calcDaysBetween() {
-      var daysBetween = 0;
-
-      for (var x = this.birthYear + 1; x <= this.currentYear - 1; x++) {
-        daysBetween += this.monthDays.reduce(function (a, b) {
-          return a + b;
-        });
-
-        if (x % 4 == 0) {
-          daysBetween += 1;
-        }
-      }
-
-      return daysBetween;
-    }
-  }, {
-    key: "calcDaysToEOY",
-    value: function calcDaysToEOY() {
-      var toEnd = this.monthDays[6] - this.birthDay;
-
-      for (var x = this.birthMonth + 1; x < this.monthDays.length; x++) {
-        toEnd += this.monthDays[x];
-      }
-
-      return toEnd;
-    }
-  }, {
-    key: "calcDaysFromBOY",
-    value: function calcDaysFromBOY() {
-      var fromBegin = 0;
-
-      for (var x = 0; x < this.currentMonth; x++) {
-        fromBegin += this.monthDays[x];
-      }
-
-      fromBegin += this.currentDate;
-
-      if (this.currentYear % 4 == 0) {
-        fromBegin += 1;
-      }
-
-      return fromBegin;
-    }
-  }, {
-    key: "sumOfDays",
-    value: function sumOfDays(a, b, c) {
-      var result = a + b + c;
-      return result;
-    }
-  }, {
-    key: "daysToWeeks",
-    value: function daysToWeeks(days) {
-      var weeks = Math.floor(days / 7);
-      return weeks;
-    }
-  }, {
-    key: "fillBlocks",
-    value: function fillBlocks(weeks) {
-      for (var x = 0; x < 4680; x++) {
-        if (x < weeks) {
-          this.container.insertAdjacentHTML("beforeend", "<div \n          style=\"background-color:red; \n          border:.3px solid #fff;\"></div>");
-        } else {
-          this.container.insertAdjacentHTML("beforeend", "<div\n          style=\"background-color:lightblue;\n          border:.3px solid #fff;\n          \"\n          ></div>");
-        }
+  calcDaysBetween() {
+    let daysBetween = 0;
+    for (var x = this.birthYear + 1; x <= this.currentYear - 1; x++) {
+      daysBetween += this.monthDays.reduce((a, b) => a + b);
+      if (x % 4 == 0) {
+        daysBetween += 1;
       }
     }
-  }]);
+    return daysBetween;
+  }
+  calcDaysToEOY() {
+    let toEnd = this.monthDays[6] - this.birthDay;
+    for (var x = this.birthMonth + 1; x < this.monthDays.length; x++) {
+      toEnd += this.monthDays[x];
+    }
+    return toEnd;
+  }
+  calcDaysFromBOY() {
+    let fromBegin = 0;
+    for (var x = 0; x < this.currentMonth; x++) {
+      fromBegin += this.monthDays[x];
+    }
+    fromBegin += this.currentDate;
+    if (this.currentYear % 4 == 0) {
+      fromBegin += 1;
+    }
+    return fromBegin;
+  }
+  sumOfDays(a, b, c) {
+    var result = a + b + c;
+    return result;
+  }
+  daysToWeeks(days) {
+    let weeks = Math.floor(days / 7);
+    return weeks;
+  }
+  fillBlocks(weeks) {
+    for (var x = 0; x < 4680; x++) {
+      if (x < weeks) {
+        this.container.insertAdjacentHTML(
+          "beforeend",
+          `<div 
+          style="background-color:red; 
+          border:.3px solid #fff;"></div>`
+        );
+      } else {
+        this.container.insertAdjacentHTML(
+          "beforeend",
+          `<div
+          style="background-color:lightblue;
+          border:.3px solid #fff;
+          "
+          ></div>`
+        );
+      }
+    }
+  }
+}
 
-  return Calc;
-}();
-
-new Calc();
+window.addEventListener("load", () => {
+  return new Calc();
+});
